7a29a2b96b120b7a320d38d7eb4f1a89a5a5b915
jshallcross <jshallcross1@icloud.com>
2021-04-25 09:14:45 -0400

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,17 @@ function Footer()
 {   
     return (
         <footer className="text-center">
-            <p>Footer</p>
+            <div className=" container p-4">
+                <a className="btn btn-outline-dark btn-lg m-2" href="https://github.com/jshallcross" target="_blank">
+                <i className="fab fa-github"></i>
+                </a>
+                <a className="btn btn-outline-dark btn-lg m-2" href="https://www.linkedin.com/in/jon-shallcross" target="_blank">
+                <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a className="btn btn-outline-dark btn-lg m-2" href="https://twitter.com/j_shallcrossdev" target="_blank">
+                <i className="fab fa-twitter"></i>
+                </a>
+            </div>
         </footer>
     );
 };
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,34 +1,9 @@
   
 import React from 'react';
 
-/*
-function Navigation(props)
-{   
-    const {navSelected , setNavSelected} = props;
-    return (
-        <div className= "collapse navbar-collapse"  id="navbarNav">
-            <ul className="navbar-nav">
-                <li className={`nav-item btn ${navSelected === "About" && "active"}`}>
-                    <span className="nav-link" onClick={()=>setNavSelected("About")}>About me</span>
-                </li>
-                <li className={`nav-item btn ${navSelected === "Projects" && "active"}`}>
-                    <span className="nav-link" onClick={()=>setNavSelected("Projects")}>Portfolio</span>
-                </li>
-                <li className={`nav-item btn ${navSelected === "Contact" && "active"}`}>
-                    <span className="nav-link" onClick={()=>setNavSelected("Contact")}>Contact</span>
-                </li>
-                <li className={`nav-item btn ${navSelected === "Resume" && "active"}`}>
-                    <span className="nav-link" onClick={()=>setNavSelected("Resume")}>Resume</span>
-                </li>
-            </ul>
-        </div>
-    );
-};
-*/
 function Navigation(props) {
     const tabs = [ 'About', 'Projects', 'Contact', 'Resume'];
     return (
-         
             <ul className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">JS</a>
                 {tabs.map(tab => (
