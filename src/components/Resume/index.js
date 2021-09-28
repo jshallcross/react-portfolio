c1647ec4992ed3b10d2442487d36d120cce3f67a
Jon Shallcross <jshallcross1@icloud.com>
2021-09-28 14:03:33 -0400

    Updates

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,7 +8,7 @@ const Resume = () => (
     </a>
     <div className="iframe-container  col-lg-12">
     <iframe title="Resume-Download" src="https://docs.google.com/document/d/1AYd5cAErYY-05dzHPjHZ9TL4nQSMn61aOVjm0Y9F8EY/edit?usp=sharing"  frameborder="0"></iframe>
-    <p className="phone">Not available to view on Phones. Use iPad or larger to view resume.</p>
+    <p className="phone">Not available to view on Phones. Please use iPad or larger to view resume.</p>
     </div>
   </div>
 );
