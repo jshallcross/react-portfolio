28f59a45f3560dbbcb303e8217f2ed6e6ac0cbc2
Jon Shallcross <jshallcross1@icloud.com>
2021-09-28 14:16:13 -0400

    Minor Updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,13 +16,19 @@ const About = () => (
       <div className="about-content ">
         <p className="about-text">
           <span className="blue" id="title">Full Stack Web Developer </span><br></br>
-          I recently received my Certificate from The University of Toronto Coding Boot Camp and am looking to move out of the hospitality industry. The hospitality industry has taught me everything you need to know about customer service, how to keep a customer happy and truly understand what a customer is looking for even when they are not clear on what they want. This is a valuable asset I will bring to the table as a web developer.
+          I recently received my Certificate from The University of Toronto Coding Boot Camp and I am now looking to transition out of the hospitality industry. 
+          The hospitality industry has taught me everything you need to know about customer service, how to keep a customer happy and truly understand what a customer is looking for even when they are not clear on what they want. 
+          This is a valuable asset I will bring to the table as a web developer.
         </p>
         <p className="about-text">
-        I currently reside in Burlington Ontario with my three kids. From March 2020 until my enrolment at the University of Toronto coding boot camp in November I have been self learning web development. Through the course I have learned HTML, CSS, JavaScript, jQuery, Bootstrap, Node.js, Express.js, AJAX, SQL, MongoDB, OOP/TDD, ORM/MVC, PWA and React.
+        I currently reside in Burlington Ontario with my three kids. 
+        From March 2020 until my enrolment at the University of Toronto coding boot camp in November of 2020,
+        I had been self learning web development. 
+        Through the course I have learned HTML, CSS, JavaScript, jQuery, Bootstrap, Node.js, Express.js, AJAX, SQL, MongoDB, OOP/TDD, ORM/MVC, PWA and React.
         </p>
         <p className="about-text">
-        The University of Toronto boot camp is the final piece of the puzzle which will allow me to make the career change I have been looking for. I hope you like the projects I have on my website and look forward to talking with you soon. 
+        The University of Toronto boot camp is the final piece of the puzzle which will allow me to make the career change I have been looking for. 
+        I hope you like the projects I have on my website and look forward to talking with you soon. 
         </p>
       </div>    
       </div>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ const Resume = () => (
       Download My Resume
     </a>
     <div className="iframe-container  col-lg-12">
-    <iframe title="Resume-Download" src="https://docs.google.com/document/d/1DKU-MI3sMbzRnugSO4sk2f5e-wEMNXISzPXeuC86sVk/edit?usp=sharing"  frameborder="0"></iframe>
+    <iframe title="Resume-Download" src="https://docs.google.com/document/d/1AYd5cAErYY-05dzHPjHZ9TL4nQSMn61aOVjm0Y9F8EY/edit?usp=sharing"  frameborder="0"></iframe>
     <p className="phone">Not available to view on Phones. Use iPad or larger to view resume.</p>
     </div>
   </div>
